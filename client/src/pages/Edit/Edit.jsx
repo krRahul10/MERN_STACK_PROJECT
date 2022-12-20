78d638dc38de53729057576b19dd4bcb9f58ce41
Rahul <rahul10kr7@gmail.com>
2022-12-20 20:51:41 +0530

    app start

---
+++ client/src/pages/Edit/Edit.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './edit.css'
+
+
+export const Edit = () => {
+  return (
+    <div>Edit</div>
+  )
+}
---
+++ client/src/pages/Profile/Profile.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './profile.css'
+
+
+export const Profile = () => {
+  return (
+    <div>Profile</div>
+  )
+}
