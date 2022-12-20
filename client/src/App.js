8d99332688188f5d501940f6cfdfe1f64984616b
Rahul <rahul10kr7@gmail.com>
2022-12-20 20:58:14 +0530

    routing done

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,27 @@
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Headers } from './Components/Headers/Headers';
+import { Route, Routes } from 'react-router-dom';
+import { Home } from './pages/Home/Home';
+import { Register } from './pages/Register/Register';
+import { Edit } from './pages/Edit/Edit';
+import { Profile } from './pages/Profile/Profile';
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello World</h1>
-
+      <Headers/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/register' element={<Register/>}/>
+        <Route path='/edit/:id' element={<Edit/>}/>
+        <Route path='/userprofile/:id' element={<Profile/>}/>
+      </Routes>
      
+
+
+
     </div>
   );
 }
