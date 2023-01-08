4d088ef4d626f98e2ca678af067ac11801c5c5fb
Rahul <rahul10kr7@gmail.com>
2023-01-08 17:49:47 +0530

    done

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const router = require("./Routes/router");
-const PORT = 6010;
+const PORT = process.env.PORT|| 6010;
 
 app.use(cors());
 app.use(express.json());
